dc620233c1b27ef97d208abbcd6226894d686d48
Ebbe Wertz <ebbe.wertz8@gmail.com>
2025-12-31 19:26:38 +0100

    Made PCM streamer configure and play on demand instead of playing a static sample

--- src/main.c
+++ src/main.c
@@ -3,18 +3,26 @@
 #include <string.h>
 #include <sys/unistd.h>
 
-#define MONO 1
-#define STEREO 2
+#define MUSIC1_PATH "romfs:/audio/song1_pcm16_44100hz_stereo.raw"
+#define MUSIC1_SAMPLE_RATE 44100.0f
+#define MUSIC1_FORMAT NDSP_FORMAT_STEREO_PCM16
 
-// sample properties
-#define SAMPLE_RATE 44100.0f //44100.0f
-#define SAMPLE_N_CHANNELS STEREO
+#define MUSIC2_PATH "romfs:/audio/song2_pcm16_48000hz_mono.raw"
+#define MUSIC2_SAMPLE_RATE 48000.0f
+#define MUSIC2_FORMAT NDSP_FORMAT_MONO_PCM16
 
-#define BYTES_PER_SAMPLE (2 * SAMPLE_N_CHANNELS) // 2 bytes for 16bit
-#define STREAM_BUF_SIZE (16*1024)
+#define STREAM_BUF_SIZE (32*1024) // about 170ms for stereo 16bit PCM at 48kHz
 #define N_BUFFERS_PER_CHANNEL 3 // 2 (dual buffering, good for sfx) or 3 (triple, more robust for music)
-
 #define AUDIO_THREAD_STACK_SIZE (1024*1024) // 1MB
+
+typedef struct {
+    const char *filePath;
+    u16 format;
+    float sampleRate;
+    bool pending;
+} AudioRequest;
+
+volatile AudioRequest currentAudioRequest = {nullptr, 0, 0, 0};
 
 typedef enum {
     BUFF_REFILL_RES_OK,
@@ -29,8 +37,21 @@ volatile bool audioThreadRunning = true;
 ndspWaveBuf ndspBuffers[N_BUFFERS_PER_CHANNEL];
 s16 *pcmBuffers[N_BUFFERS_PER_CHANNEL];
 
+void play(const char* filePath, const u16 format, const float sampleRate) {
+    currentAudioRequest.filePath = filePath;
+    currentAudioRequest.format = format;
+    currentAudioRequest.sampleRate = sampleRate;
+    currentAudioRequest.pending = true;
+    LightEvent_Signal(&audioRequestEvent);
+}
+
+inline int getBytesPerSample(const u16 format) {
+    const int nChannels = format & 0x03; // 0x01 = mono, 0x02 = stereo (mask=0x03)
+    const int bytesPerChannel = format & 0x04 ? 2 : 1; // 0x00 = 8bit, 0x04 = 16bit (mask=0x04)
+    return nChannels * bytesPerChannel;
+}
+
 bufferRefillResult fillBufferFromFile(const int buffer_index, FILE *file) {
-    printf("filling buffer %d\n", buffer_index);
     // read new PCM data from file
     const size_t bytesRead = fread(pcmBuffers[buffer_index], 1,STREAM_BUF_SIZE, file);
     if (bytesRead == 0) {
@@ -39,7 +60,7 @@ bufferRefillResult fillBufferFromFile(const int buffer_index, FILE *file) {
     }
     // update buffer struct
     ndspBuffers[buffer_index].data_pcm16 = pcmBuffers[buffer_index];
-    ndspBuffers[buffer_index].nsamples = bytesRead / BYTES_PER_SAMPLE;
+    ndspBuffers[buffer_index].nsamples = bytesRead / getBytesPerSample(currentAudioRequest.format);
     // ensure buffer is actually in memory and not in CPU cache
     const Result flushResult = DSP_FlushDataCache(pcmBuffers[buffer_index], bytesRead);
     if (flushResult) return BUFF_REFILL_RES_MEMORY_FLUSH_ERROR;
@@ -73,13 +94,13 @@ bool initBuffers() {
     return true;
 }
 
-void initChannel() {
+void setupChannel(const u16 format, const float sampleRate) {
     ndspSetOutputMode(NDSP_OUTPUT_STEREO);
     ndspChnReset(0);
     ndspChnInitParams(0);
     ndspChnSetInterp(0, NDSP_INTERP_POLYPHASE);
-    ndspChnSetRate(0, SAMPLE_RATE);
-    ndspChnSetFormat(0, SAMPLE_N_CHANNELS == STEREO ? NDSP_FORMAT_STEREO_PCM16 : NDSP_FORMAT_MONO_PCM16);
+    ndspChnSetRate(0, sampleRate);
+    ndspChnSetFormat(0, format);
 }
 
 void audioCallback([[maybe_unused]] void* unused_) {
@@ -87,38 +108,48 @@ void audioCallback([[maybe_unused]] void* unused_) {
 }
 
 
-inline void handleThreadRequest(FILE* pcmDataFileHandle) {
+inline void handleThreadRequest(FILE** pcmFile) {
+
+    if (currentAudioRequest.pending) {
+        printf("play requested\n");
+        currentAudioRequest.pending = false;
+        // setup channel
+        setupChannel(currentAudioRequest.format, currentAudioRequest.sampleRate);
+        // open file
+        *pcmFile = fopen(currentAudioRequest.filePath, "rb");
+        if (!*pcmFile) {
+            printf("Failed to open PCM file: %s\n", currentAudioRequest.filePath);
+            sleep(3);
+            return;
+        }
+        // prefill buffers
+        for (int i = 0; i < N_BUFFERS_PER_CHANNEL; i++) {
+            printf("buff %d prefill\n", i);
+            const bufferRefillResult res = fillBufferFromFile(i, *pcmFile);
+            printRefillResult(res);
+            if (res == BUFF_REFILL_RES_FILEREAD_EOF)
+                fclose(*pcmFile);
+        }
+    }
+
     for (int i = 0; i < N_BUFFERS_PER_CHANNEL; i++) {
         if (ndspBuffers[i].status == NDSP_WBUF_DONE) {
-            const bufferRefillResult res = fillBufferFromFile(i, pcmDataFileHandle);
+            printf("buff %d refill\n", i);
+            const bufferRefillResult res = fillBufferFromFile(i, *pcmFile);
             printRefillResult(res);
         }
     }
 }
 
 void audioThread([[maybe_unused]] void* unused_) {
-    printf("audio thread started\n");
-
-    // open file handle
-    FILE *pcmDataFileHandle = fopen("romfs:/audio/song1_pcm16_44100hz_stereo.raw", "rb");
-    if (!pcmDataFileHandle) {
-        printf("Failed to open PCM file\n");
-        sleep(3);
-        return;
-    }
-
-    // prefill buffers
-    for (int i = 0; i < N_BUFFERS_PER_CHANNEL; i++) {
-        const bufferRefillResult res = fillBufferFromFile(i, pcmDataFileHandle);
-        printRefillResult(res);
-    }
+    FILE *currentPcmFile = nullptr;
+    printf("Audio thread ready\n");
 
     // main audio thread loop
     while (audioThreadRunning) {
-        handleThreadRequest(pcmDataFileHandle);
+        handleThreadRequest(&currentPcmFile);
         LightEvent_Wait(&audioRequestEvent);
     }
-    fclose(pcmDataFileHandle);
 }
 
 
@@ -136,7 +167,6 @@ int main(void) {
         return 1;
     }
 
-    initChannel();
     ndspSetCallback(audioCallback, nullptr);
 
     int32_t priority = 0x30; // main thread priority
@@ -149,7 +179,15 @@ int main(void) {
     // main loop
     while (aptMainLoop()) {
         hidScanInput();
-        if (hidKeysDown() & KEY_START) break;
+        u32 keysdown = hidKeysDown();
+        if (keysdown & KEY_START) break;
+        if (keysdown & KEY_A) {
+            printf("requesting play song 1\n");
+            play(MUSIC1_PATH, MUSIC1_FORMAT, MUSIC1_SAMPLE_RATE);
+        } else if (keysdown & KEY_B) {
+            printf("requesting play song 2\n");
+            play(MUSIC2_PATH, MUSIC2_FORMAT, MUSIC2_SAMPLE_RATE);
+        }
         gspWaitForVBlank();
         gfxSwapBuffers();
     }
